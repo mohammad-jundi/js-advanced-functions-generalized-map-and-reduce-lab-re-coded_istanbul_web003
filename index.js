9b8c2ad3e7de087688c22897c3cce2a63921c48b
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-08-22 10:43:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // Add your functions here
-adsfmb.hf keQ
+function map(str , fn ){   
+    let result = []
+    for (let i = 0; i< str.length ; i++ ){
+        result.push(fn(str[i]));
+    }
+    return result
+}
+
+function reduce(str, fn, start) {
+    const k = Object.keys(str)
+    let i = 1 
+
+    if(start){
+        i = 0
+    } else {
+        start = str[k[0]]
+    }
+
+    for(i; i < k.length; i++){
+        start = fn(start, str[k[i]])
+    }
+    return start
+}
